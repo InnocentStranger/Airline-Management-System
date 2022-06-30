67716bb66db1cc6ae2e11bdedda1096cf6c46a60
Aryan <aryancoder440@gmail.com>
2022-06-30 17:42:59 +0530

    Added All flight Functionality

--- flight.cpp
+++ flight.cpp
@@ -17,4 +17,27 @@ using namespace std;
         cout << "Enter Flight Cost:"; cin >> cost;
     }
 
+    void flight::displayFlightDetails(){
+
+       cout << "\t"; cout << "  " <<
+       left << setw(15) << fNo  << "| " 
+       << setw(15) << from  << "| "  << 
+       setw(15) << to  << "| "  
+       << setw(15) << dt <<  "| "  << 
+       setw(15) << at <<  "| " 
+       << setw(15) << date <<  "| "  <<
+       setw(15) << cost << " " << endl;
+    }
+    bool flight::searchFlight(char to[],char from[]){
+            if(strcmp(this->to,to)==0 && strcmp(this->from,from)==0) return true;
+            return false;
+    }
+    bool flight::operator == (flight &f){
+        if(this->fNo == f.fNo) return true;
+        return false;
+    }
+    bool flight::cmpFno(int fNo){
+        if(this->fNo == fNo) return true;
+        return false;
+    }
 #endif

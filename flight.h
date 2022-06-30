2380fb8802c23fe9a86a379cc4e7f1fedf7894e4
Aryan <aryancoder440@gmail.com>
2022-06-30 17:43:28 +0530

    Updated Properties and Methods

--- flight.h
+++ flight.h
@@ -5,13 +5,17 @@ using namespace std;
 
 class flight{
     // Properties
-    string from,to,at,dt,date;
+    char from[20],to[20],at[20],dt[20],date[20];
     int cost,fNo;
     public:
-        flight(){cost = 0;fNo=0;}
+        flight(){cost = 0;fNo=0;from[0]='\0';to[0]='\0';at[0]='\0';dt[0]='\0';date[0]='\0';}
         //methods
         void inputFlightDetails(); // Done
         void displayFlightDetails();
+        bool searchFlight(char[],char[]);
+        bool operator == (flight &f);
+        bool cmpFno(int fNo);
+        friend class user;
 };
 
 #endif

fa4283d19d3e7aa3a029f9ec00c7a24199bb9c27
Aryan <aryancoder440@gmail.com>
2022-06-30 17:38:00 +0530

    Added functionality to update Flight No

--- user.h
+++ user.h
@@ -6,15 +6,19 @@ using  namespace std;
 
 class user{
     //Properties
-    string fname,lname,password;
+    char fname[20],lname[20],password[20];
     int phoneNo,passNo,fNo;
     public:
-        user(){phoneNo = 0,passNo = 0;fNo=0;}
+        user(){phoneNo = 0,passNo = 0;fNo=0;fname[0]='\0';lname[0]='\0';password[0]='\0';}
         //Methods
         void enterUserInfo();
         void displayUserInfo(flight&);
-        bool login(int,string);
-
-};
+        bool login(int,char[]);
+        // Operaotr overloading
+        bool operator == (user const &obj);
+        void updatefNo(flight&);
+        void updatefNo(int);
+        int retrieve();
+}users;
 
 #endif
